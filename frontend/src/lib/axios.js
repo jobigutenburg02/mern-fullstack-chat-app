a7c65dd4413a200aa2be1b4ca36190000dcfe6d7
Johan Biju George <141867485+jobigutenburg02@users.noreply.github.com>
2025-07-13 16:18:05 +0400

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,4 +1,4 @@
-const MODE = import.meta.env.MODE || 'production' // fallback if undefined
+const MODE = import.meta.env.MODE || 'production'
 
 export const axiosInstance = axios.create({
   baseURL: MODE === 'development' ? "http://localhost:3000/api" : "/api",
