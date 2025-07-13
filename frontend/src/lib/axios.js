49a28752c1f8a85e9515a77afb4566dabea990d2
jobigutenburg02 <jbros2513@gmail.com>
2025-07-13 16:23:07 +0400

    base url modified

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
-import axios from 'axios'
+const MODE = import.meta.env.MODE || 'production'
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === 'development' ? "http://localhost:3000/api": "/api",
-    withCredentials: true, // send cookies in every request
+  baseURL: MODE === 'development' ? "http://localhost:3000/api" : "/api",
+  withCredentials: true,
 })
